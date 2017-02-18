30926b494aa677d654c4b1191fffcc03fcbd1647
yuhan lin <yuhan lin>
2017-02-18 17:35:58 -0500

    Added LRTable class to LRHelper

--- C++/LRHelper.cpp
+++ C++/LRHelper.cpp
@@ -8,13 +8,9 @@ LRItem LRItem::advance(){
     return item;
 }
 
-bool LRItem::operator==(const LRItem &other){
-    return (prodPos==other.prodPos && dotPos==other.dotPos && lhs==other.lhs);
+bool operator==(const LRItem &x, const LRItem &y){
+    return (y.prodPos==x.prodPos && y.dotPos==x.dotPos && y.lhs==x.lhs);
 }
-
-// size_t LRItemHash::operator()(const LRItem &item){
-//     return item.dotPos ^ item.prodPos;
-// }
 
 void LRStateSet::newState(){
     length++;
@@ -29,7 +25,64 @@ size_t LRStateSet::size(){
 std::unordered_set<LRItem>& LRStateSet::kernelState(int index){
     return kernelSet[index];
 }
-
 std::vector<LRItem>& LRStateSet::closureState(int index){
     return closureSet[index];
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+size_t LRTable::size(){
+    return length;
+}
+
+LRTable::LRTable(int symbols){
+    symbolCount = symbols;
+}
+
+LRTable::~LRTable(){
+    for (int* row : transitions)
+        delete[] row;
+    for (int* reduction : reductions)
+        delete[] reduction;
+}
+
+//Add new state in parse table
+void LRTable::newState(){
+    length++;
+    //Initiate new parse table entry
+    transitions.push_back(new int[symbolCount]);
+    reductions.push_back(new int[3]);
+    //Set all values in new entry to -1 as default
+    for (int i=0; i<symbolCount; i++){
+        transitions.back()[i] = -1;
+    }
+    reductions.back()[0] = -1;
+    reductions.back()[1] = -1;
+    reductions.back()[2] = -1;
+}
+
+//Make last state reduction state
+void LRTable::reduceState(int prodPos, int lhs, int prodNum){
+    //Set entire table row to -2 for reduction
+    for (int i=0; i<symbolCount; i++){
+        transitions.back()[i] = -2;
+    }
+    //Set reduction parameters
+    reductions.back()[0] = prodPos;
+    reductions.back()[1] = lhs;
+    reductions.back()[2] = prodNum;
+}
+
+//Reduction parameter accessors with expressive syntax
+int& LRTable::prodPos(int state){
+    return reductions[state][0];
+}
+int& LRTable::lhsNum(int state){
+    return reductions[state][1];
+}
+int& LRTable::prodNum(int state){
+    return reductions[state][2];
+}
+int& LRTable::operator()(int state, int symbolNum){
+    return transitions[state][symbolNum];
 }
--- C++/LRHelper.h
+++ C++/LRHelper.h
@@ -9,14 +9,16 @@ struct LRItem{
     int prodPos;
     //The nth symbol behind the "dot"
     int dotPos;
-    //LHS of item's production
+    //LHS of item's production (incremented)
     int lhs;
     //Return copy of item shifted by one
     LRItem advance(); 
-    //For the hashset
-    bool operator==(const LRItem& other);
 };
 
+//For the hashset
+bool operator==(const LRItem&, const LRItem&);
+
+//Hash function for LR Items
 namespace std {
     template <>
         class hash<LRItem>{
@@ -45,4 +47,38 @@ public:
     //Returns a specified kernel/closure state in the state set
     std::unordered_set<LRItem>& kernelState(int index);
     std::vector<LRItem>& closureState(int index);
+};
+
+//Represents LR parse table
+class LRTable {
+    //Vector of int arrays to represent the transition table of states to symbols
+    //-1 is no transition, -2 is reduction, +ve indicates which state to shift to
+    std::vector<int*> transitions;
+    //Represent the reduction attributes of each state, if any. 
+    //[0] is production position, [1] is the lhs, [2] is the production number 
+    std::vector<int*> reductions;
+    //Length of table
+    size_t length = 0;
+    //# of symbols in the grammar
+    int symbolCount;
+public:
+    size_t size();
+    //Initializes symbol Count
+    LRTable(int symbols);
+    ~LRTable();
+    //Delete copy and assignment constructors
+    LRTable(LRTable&) = delete;
+    LRTable &operator=(LRTable&) = delete;
+    //Add new state to table
+    void newState();
+    //Turn the last state into a reduction state
+    void reduceState(int prodPos, int lhs, int prodNum);
+    //Returns production position in reducing state
+    int& prodPos(int state);
+    //Return reduction lhs num reference for a state
+    int& lhsNum(int state);
+    //Return reduction production num reference for state
+    int& prodNum(int state);
+    //Return the transition of a state for a given symbol
+    int& operator()(int state, int symbolNum);
 };
